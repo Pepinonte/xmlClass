76e386709f2d535c5199d8c9e21c12b32e4c35ff
GROS Christian <christian-gros@outlook.fr>
2021-03-02 15:19:58 +0100

    ajout et sup de quelques methodes (probleme dans la methode mod)

--- testXml/main.cpp
+++ testXml/main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
 
     xml.openFile("C:/Users/Christian GROS/Desktop/xmlClass/test3.xml");
 //    xml.readElement("dateDepart");
-//    xml.modElement("dateDepart");
-    xml.supElement("seance");
+    xml.modElement("dateDepart");
+//    xml.supElement("seance");
 //    xml.newSeance("a","b","c","c","d");
 //    xml.newSeance("a","b","c","c","d");
-//    xml.newSeance("a","b","c","c","d");
+    xml.newSeance("a","b","c","c","d");
 
     return a.exec();
 }
